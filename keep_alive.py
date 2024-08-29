d17924a1011c04623ef7f4b69c755749cabe9bf2
synapsephotos <135157942+synapsephotos@users.noreply.github.com>
2024-08-29 17:32:23 +0000

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,13 +1,10 @@
 from flask import Flask
-from threading import Thread
-
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST', 'CONNECT', 'PUT', 'DELETE', 'PATCH', 'OPTIONS', 'TRACE', 'HEAD'])
+@app.route('/')
+def hello_world():
+    return 'Hello from Koyeb'
 
-def run():
-  app.run(host='0.0.0.0', port=3000, debug=False, use_reloader=False)
 
 def keep_alive():
-  server = Thread(target=run)
-  server.start()
+  app.run()
